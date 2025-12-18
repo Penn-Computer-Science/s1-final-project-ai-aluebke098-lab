ddfc8cb0ef5923d6db00f0f56159e223daa67dd0
aluebke098-lab <aluebke098@phm.k12.in.us>
2025-12-18 09:45:28 -0500

    trying to get two snakes in one display.
    proving harder than I thought. needs a lot more work. current issue finding the snake for the model.

--- snake_game_env.py
+++ snake_game_env.py
@@ -29,33 +29,24 @@ RED = (200,0,0)
 BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
 
-BLOCK_SIZE = 20
+BLOCK_SIZE = 40
 SPEED = 20
 
-class SnakeGameAI:
+
+class Viper:
     
     def __init__(self, w=640, h=480):
         self.w = w
         self.h = h
-        # init display
-        self.display = pygame.display.set_mode((self.w, self.h))
-        pygame.display.set_caption('Snake')
-        self.clock = pygame.time.Clock()
-        self.reset()
+    #     # init display
+    #     self.display = pygame.display.set_mode((self.w, self.h))
+    #     pygame.display.set_caption('Snake')
+    #     self.clock = pygame.time.Clock()
+    #     self.reset()
 
-    def reset(self):
-        # init game state
-        self.direction = Direction.LEFT
+    # def reset(self):
+    #     # init game state
         
-        self.head = Point(self.w*2/3, self.h*2/3)
-        self.snake = [self.head, 
-                      Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
-        
-        self.score = 0
-        self.food = None
-        self._place_food()
-        self.frame_iteration = 0
         
     def _place_food(self):
         x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
@@ -64,39 +55,39 @@ class SnakeGameAI:
         if self.food in self.snake:
             self._place_food()
         
-    def play_step(self, action):
-        self.frame_iteration += 1
-        # 1. collect user input
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+    # def play_step(self, action):
+    #     self.frame_iteration += 1
+    #     # 1. collect user input
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             pygame.quit()
+    #             quit()
         
-        # 2. move
-        self._move(action) # update the head
-        self.snake.insert(0, self.head)
+    #     # 2. move
+    #     self._move(action) # update the head
+    #     self.snake.insert(0, self.head)
         
-        # 3. check if game over
-        reward = 0
-        game_over = False
-        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
-            game_over = True
-            reward = -10
-            return reward, game_over, self.score
+    #     # 3. check if game over
+    #     reward = 0
+    #     game_over = False
+    #     if self.is_collision() or self.frame_iteration > 100*len(self.snake):
+    #         game_over = True
+    #         reward = -10
+    #         return reward, game_over, self.score
             
-        # 4. place new food or just move
-        if self.head == self.food:
-            self.score += 1
-            reward = 10
-            self._place_food()
-        else:
-            self.snake.pop()
+    #     # 4. place new food or just move
+    #     if self.head == self.food:
+    #         self.score += 1
+    #         reward = 10
+    #         self._place_food()
+    #     else:
+    #         self.snake.pop()
         
-        # 5. update ui and clock
-        self._update_ui()
-        self.clock.tick(SPEED)
-        # 6. return game over and score
-        return reward, game_over, self.score
+    #     # 5. update ui and clock
+    #     self._update_ui()
+    #     self.clock.tick(SPEED)
+    #     # 6. return game over and score
+    #     return reward, game_over, self.score
     
     def is_collision(self, pt=None):
         if pt is None:
@@ -105,23 +96,23 @@ class SnakeGameAI:
         if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
             return True
         # hits itself
-        if pt in self.snake[1:]:
+        if pt in self.snake[1:] or pt in SnakeGameAI.snake:
             return True
         
         return False
         
-    def _update_ui(self):
-        self.display.fill(BLACK)
+    # def _update_ui(self):
+    #     self.display.fill(BLACK)
         
-        for pt in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+    #     for pt in self.snake:
+    #         pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+    #         pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
             
-        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+    #     pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
         
-        text = font.render("Score: " + str(self.score), True, WHITE)
-        self.display.blit(text, [0, 0])
-        pygame.display.flip()
+    #     text = font.render("Score: " + str(self.score), True, WHITE)
+    #     self.display.blit(text, [0, 0])
+    #     pygame.display.flip()
         
     def _move(self, action):
         # [straight, right, left]
@@ -154,12 +145,10 @@ class SnakeGameAI:
         self.head = Point(x, y)
 
 
+viper = Viper()
 
 
-
-
-
-class Viper:
+class SnakeGameAI:
     
     def __init__(self, w=640, h=480):
         self.w = w
@@ -169,13 +158,22 @@ class Viper:
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
         self.reset()
+        self.snake = [0]
+        viper.snake = [0]
 
     def reset(self):
         # init game state
-        self.direction = Direction.RIGHT
+        self.direction = Direction.LEFT
         
-        self.head = Point(self.w/3, self.h/3)
+        self.head = Point(self.w*2/3, self.h*2/3)
         self.snake = [self.head, 
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
+        viper.direction = Direction.RIGHT
+        
+        viper.head = Point(self.w/3, self.h/3)
+        viper.snake = [self.head, 
                       Point(self.head.x-BLOCK_SIZE, self.head.y),
                       Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
         
@@ -183,6 +181,11 @@ class Viper:
         self.food = None
         self._place_food()
         self.frame_iteration = 0
+
+        viper.score = 0
+        viper.food = None
+        viper._place_food()
+        viper.frame_iteration = 0
         
     def _place_food(self):
         x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
@@ -202,11 +205,18 @@ class Viper:
         # 2. move
         self._move(action) # update the head
         self.snake.insert(0, self.head)
+
+        viper._move(action)
+        viper.snake.insert(0, viper.head)
         
         # 3. check if game over
         reward = 0
         game_over = False
         if self.is_collision() or self.frame_iteration > 100*len(self.snake):
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
+        if viper.is_collision():
             game_over = True
             reward = -10
             return reward, game_over, self.score
@@ -218,6 +228,11 @@ class Viper:
             self._place_food()
         else:
             self.snake.pop()
+        if viper.head == viper.food:
+            viper.score += 1
+            viper._place_food()
+        else:
+            viper.snake.pop()
         
         # 5. update ui and clock
         self._update_ui()
@@ -232,7 +247,7 @@ class Viper:
         if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
             return True
         # hits itself
-        if pt in self.snake[1:]:
+        if pt in self.snake[1:] or pt in viper.snake:
             return True
         
         return False
@@ -243,11 +258,17 @@ class Viper:
         for pt in self.snake:
             pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
             pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+        for pt in viper.snake:
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
             
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(self.display, RED, pygame.Rect(viper.food.x, viper.food.y, BLOCK_SIZE, BLOCK_SIZE))
         
         text = font.render("Score: " + str(self.score), True, WHITE)
+        text_v = font.render("Score: " + str(viper.score), True, WHITE)
         self.display.blit(text, [0, 0])
+        self.display.blit(text_v, [300, 0])
         pygame.display.flip()
         
     def _move(self, action):
--- snake_saver_main.py
+++ snake_saver_main.py
@@ -17,71 +17,71 @@ def train():
     game = SnakeGameAI()
     done = False
 
-    plot_scores_v = [] 
-    plot_mean_scores_v = [] 
-    total_score_v = 0 
-    record_v = 0 
-    agent_v = Agent_V()
-    game_v = Viper()
-    done_v = False
+    # plot_scores_v = [] 
+    # plot_mean_scores_v = [] 
+    # total_score_v = 0 
+    # record_v = 0 
+    # agent_v = Agent_V()
+    # game_v = Viper()
+    # done_v = False
 
     while True:
         # get old state
         state_old = agent.get_state(game)
-        state_old_v = agent_v.get_state(game_v)
+        # state_old_v = agent_v.get_state(game_v)
 
         # get move
         final_move = agent.get_action(state_old)
-        final_move_v = agent_v.get_action(state_old_v)
+        # final_move_v = agent_v.get_action(state_old_v)
 
         # perform move and get new state
         if not done:
             reward, done, score = game.play_step(final_move)
-        if not done_v:
-            reward_v, done_v, score_v = game_v.play_step(final_move)
+        # if not done_v:
+        #     reward_v, done_v, score_v = game_v.play_step(final_move)
 
         state_new = agent.get_state(game)
-        state_new_v = agent.get_state(game_v)
+        # state_new_v = agent.get_state(game_v)
 
         #train short memory (only for one step)
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
-        agent_v.train_short_memory(state_old_v, final_move_v, reward_v, state_new_v, done_v)
+        # agent_v.train_short_memory(state_old_v, final_move_v, reward_v, state_new_v, done_v)
 
         #remember
         agent.remember(state_old, final_move, reward, state_new, done)
-        agent_v.remember(state_old_v, final_move_v, reward_v, state_new_v, done_v)
+        # agent_v.remember(state_old_v, final_move_v, reward_v, state_new_v, done_v)
 
-        if done or done_v:
+        if done:
             # train long memory - aka replay memory aka experience replay - train based on all prior information
             game.reset()
-            game_v.reset()
+            # game_v.reset()
             agent.n_games += 1
-            agent_v.n_games += 1
+            # agent_v.n_games += 1
             agent.train_long_memory()
-            agent_v.train_long_memory()
+            # agent_v.train_long_memory()
 
             done = False
-            done_v = False
+            # done_v = False
 
             if score > record: # update high score
                 record = score
                 agent.model.save()
-            if score_v > record_v:
-                record_v = score_v
-                agent_v.model.save()
+            # if score_v > record_v:
+            #     record_v = score_v
+            #     agent_v.model.save()
 
             print('Snake: Game', agent.n_games, 'Score', score, 'Record:', record)
-            print('Viper: Game', agent_v.n_games, 'Score', score_v, 'Record:', record_v)
+            # print('Viper: Game', agent_v.n_games, 'Score', score_v, 'Record:', record_v)
 
             plot_scores.append(score)
-            plot_scores_v.append(score_v)
+            # plot_scores_v.append(score_v)
             total_score += score
-            total_score_v += score_v
+            # total_score_v += score_v
             mean_score = total_score / agent.n_games
-            mean_score_v = total_score_v / agent_v.n_games
+            # mean_score_v = total_score_v / agent_v.n_games
             plot_mean_scores.append(mean_score)
-            plot_mean_scores_v.append(mean_score_v)
-            make_graphs(plot_scores, plot_mean_scores, plot_scores_v, plot_mean_scores_v)
+            # plot_mean_scores_v.append(mean_score_v)
+            make_graphs(plot_scores, plot_mean_scores)
 
 
 if __name__ == '__main__':
